e321a98338a553b9af42ad241d4dc688b5aaebad
JohnyXu <ahstuxq@163.com>
2021-05-15 11:16:11 +1000

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fb06401c9793e26f7781875f3239677e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c2566405-6442-4d32-8d24-fc6adddce04a'
+};
